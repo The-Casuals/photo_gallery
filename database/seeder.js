3fa658f457315c9b06f430b8662499b024cf2cb1
Jacky Xia <jacky.w.xia@gmail.com>
2021-01-14 12:28:13 -0800

    Implement functions within my seeder to make it more clean and to have different random values

--- database/seeder.js
+++ database/seeder.js
@@ -1,33 +1,39 @@
 const faker = require('faker');
 const { Gallery } = require('./index.js');
 
-
 const randomImageUrl = 'https://loremflickr.com/1080/720/real_estate';
 
-const galleries = [];
+const getGalleries = () => {
+  const galleries = [];
+  let randomLength = Math.floor(Math.random() * (50 - 15) + 15);
+  for (let i = 0; i < randomLength; i++) {
+    galleries.push({
+      id: i + 1,
+      photoName: faker.random.words(),
+      photoUrl: randomImageUrl,
+      photoDescription: faker.lorem.paragraph(),
+    });
+  }
+  return galleries;
+};
 
-for (let i = 0; i < 20; i++) {
-  galleries.push({
-    id: i + 1,
-    photoName: faker.random.words(),
-    photoUrl: randomImageUrl,
-    photoDescription: faker.lorem.paragraph(),
-  });
-}
+const getSampleData = () => {
+  for (let i = 0; i < 100; i++) {
+    const gallerymodel = {
+      id: i + 1,
+      title: faker.lorem.sentence(),
+      reviews: Math.floor(Math.random() * (50 - 10) + 10),
+      rating: Math.random() * (5.0 - 4.0) + 4,
+      isSuperhost: faker.random.boolean(),
+      location: {
+        city: faker.address.city(),
+        state: faker.address.state(),
+        country: faker.address.country(),
+      },
+      gallery: getGalleries(),
+    };
+    Gallery.insertMany(gallerymodel);
+  }
+};
 
-for (let i = 0; i < 100; i++) {
-  const gallerymodel = {
-    id: i + 1,
-    title: faker.lorem.sentence(),
-    reviews: Math.floor(Math.random() * (50 - 10) + 10),
-    rating: Math.random() * (5.0 - 4.0) + 4,
-    isSuperhost: faker.random.boolean(),
-    location: {
-      city: faker.address.city(),
-      state: faker.address.state(),
-      country: faker.address.country(),
-    },
-    gallery: galleries,
-  };
-  Gallery.insertMany(gallerymodel);
-}
+getSampleData();
